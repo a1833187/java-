662bce40d452fb55b2ed50338eb416c04141df57
邱瑞翔 <1330342818@qq.com>
2021-12-02 23:17:12 +0800

    刷题！

---
+++ leedcode219/Solution.java
@@ -0,0 +1,27 @@
+package com.qrxedu.leedcode219;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * 给定一个整数数组和一个整数k，判断数组中是否存在两个不同的索引i和j，
+ * 使得nums [i] = nums [j]，并且 i 和 j的差的 绝对值 至多为 k。
+
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < nums.length;i++) {
+            if (!queue.isEmpty() && queue.contains(nums[i])) {
+                return true;
+            }
+            queue.addLast(nums[i]);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+            return false;
+    }
+}
---
+++ leedcode506/Solution.java
@@ -0,0 +1,34 @@
+package com.qrxedu.leedcode506;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        String[] first = {"Gold Medal","Silver Medal","Bronze Medal"};
+        Map<Integer,Integer> map = new HashMap<>();
+        int[] arr = Arrays.copyOf(score,score.length);
+        Arrays.sort(arr);
+        for(int i = 0; i < arr.length;i++){
+            map.put(arr[i],arr.length-i-1);
+        }
+        String[] out = new String[arr.length];
+        for(int i = 0; i < arr.length;i++) {
+            int rank = map.get(score[i]);
+            out[i] = rank < 3? first[rank]:Integer.toString(rank+1);
+        }
+        return out;
+    }
+    @Test
+    public void show(){
+        System.out.println(Arrays.toString(findRelativeRanks(new int[]{10,3,8,9,4})));
+    }
+}
