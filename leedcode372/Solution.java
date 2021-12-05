580d44345bccf1c224dca6e90fc16a654b1bcb05
邱瑞翔 <1330342818@qq.com>
2021-12-05 18:58:35 +0800

    刷题

---
+++ leedcode372/Solution.java
@@ -0,0 +1,43 @@
+package com.qrxedu.leedcode372;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public static final int MOD = 1337;
+
+    public int superPow(int a, int[] b) {
+        int res = 1;
+        for (int i = b.length - 1; i >= 0; i--) {
+            res = (int) (((long) res * pow(a, b[i])) % MOD);
+            a = pow(a, 10);
+        }
+        return res;
+    }
+
+    public int pow(int a, int n) {
+        //快速幂取模
+        int res = 1;
+        while (n != 0) {
+            if (n % 2 != 0) {
+                res = (int) (((long) res * a) % MOD);
+            }
+            a = (int) (((long) a * a) % MOD);
+            n = n / 2;
+        }
+        return res;
+    }
+    public int fastPower(int a,int n){
+        //快速幂(O(logn))
+        int res = 1;
+        while (n != 0) {
+            if (n % 2 != 0) {
+                res = (int) (((long) res * a));
+            }
+            a = (int) (((long) a * a));
+            n = n / 2;
+        }
+        return res;
+    }
+}
