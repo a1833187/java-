3ba677b365ab2c686e4ed8a65e24b66e875bd531
邱瑞翔 <1330342818@qq.com>
2022-01-17 21:17:42 +0800

    数组

---
+++ lcseries/leedcode189/TestDemo.java
@@ -0,0 +1,15 @@
+package com.qrxedu.leedcode189;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1,2,3,4,5,6};
+        int k = 4;
+        solution.rotate(nums,k);
+
+    }
+}
