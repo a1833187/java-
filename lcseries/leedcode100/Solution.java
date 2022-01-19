bc2782179be9d4a44f41b3037a5153878ec36a3d
邱瑞翔 <1330342818@qq.com>
2022-01-19 17:05:39 +0800

    相同二叉树

---
+++ lcseries/leedcode100/Solution.java
@@ -0,0 +1,33 @@
+package com.qrxedu.leedcode100;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }else if(p == null || q== null){
+            return false;
+        }else if(p.val!=q.val){
+            return false;
+        }else{
+            boolean leftT = isSameTree(p.left,q.left);
+            boolean rightT = isSameTree(p.right,q.right);
+            return leftT && rightT;
+        }
+    }
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
+
+
