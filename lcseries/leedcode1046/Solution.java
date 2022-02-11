4cb676736d31c1f9b285f30949c57abfd4237bc2
邱瑞翔 <1330342818@qq.com>
2022-02-11 17:49:16 +0800

    homework

---
+++ lcseries/leedcode1046/Solution.java
@@ -0,0 +1,34 @@
+package com.qrxedu.leedcode1046;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        for(int num : stones){
+            priorityQueue.add(num);
+        }
+        while(priorityQueue.size() > 1){
+            int top1 = priorityQueue.poll();
+            int top2 = priorityQueue.poll();
+            if(top1 > top2){
+                priorityQueue.offer(top1 - top2);
+            }
+        }
+        if(priorityQueue.size() == 1){
+            return priorityQueue.peek();
+        }else{
+            return 0;
+        }
+    }
+}
