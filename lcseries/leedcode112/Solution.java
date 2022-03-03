a8518c0ef6ca0b762ba8ad13346d1e2120cb88cf
邱瑞翔 <1330342818@qq.com>
2022-03-03 17:04:43 +0800

    dfs

---
+++ lcseries/leedcode112/Solution.java
@@ -0,0 +1,45 @@
+package qrxedu.leedcode112;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+    int count = 0;
+    boolean flag = false;
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null){
+            return false;
+        }
+        count+=root.val;
+        hasPath(root,targetSum);
+        return flag;
+    }
+    public void hasPath(TreeNode root, int targetNum){
+        if(root == null){
+            return;
+        }
+        if(root.left != null){
+            count+=root.left.val;
+            hasPath(root.left,targetNum);
+        }
+        if(root.right != null){
+            count+=root.right.val;
+            hasPath(root.right,targetNum);
+        }
+        if(root.left == null && root.right == null && count == targetNum){
+            flag = true;
+            return;
+        }
+        count -= root.val;
+    }
+}
