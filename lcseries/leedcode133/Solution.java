c1e5e56580103c46d7b11efb793bfb591bc03a59
邱瑞翔 <1330342818@qq.com>
2022-03-10 19:57:49 +0800

    cloneGraph

---
+++ lcseries/leedcode133/Solution.java
@@ -0,0 +1,44 @@
+package qrxedu.leedcode133;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+    public Map<Node,Node> isVisited = new HashMap<>();
+    public Node cloneGraph(Node node) {
+        if(node == null){
+            return null;
+        }
+        if(isVisited.containsKey(node)){
+            return isVisited.get(node);
+        }
+        Node newNode = new Node(node.val,new ArrayList<>());
+        isVisited.put(node,newNode);
+        for(Node n : node.neighbors){
+            newNode.neighbors.add(cloneGraph(n));
+        }
+        return newNode;
+    }
+}
