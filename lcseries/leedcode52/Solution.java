a084da2518a6f7eac405871ce784ad6906fbb036
邱瑞翔 <1330342818@qq.com>
2022-03-24 17:02:30 +0800

    回溯(N皇后II)

---
+++ lcseries/leedcode52/Solution.java
@@ -0,0 +1,35 @@
+package qrxedu.leedcode52;
+
+/**
+ * 统计N皇后的排列数量
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public int count = 0;
+    public int totalNQueens(int n){
+        int[] arr = new int[n];
+        dfs(arr,0);
+        return count;
+    }
+    public void dfs(int[] arr,int index){
+        if(index == arr.length){
+            count++;
+            return;
+        }
+        for(int i = 0; i < arr.length; i++){
+            arr[index] = i;
+            if(isValid(arr,index)){
+                dfs(arr,index+1);
+            }
+        }
+    }
+    public boolean isValid(int[] arr,int x){
+        for(int i = 0; i < x; i++){
+            if(arr[i] == arr[x] || Math.abs(arr[i]-arr[x]) == Math.abs(i-x)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
