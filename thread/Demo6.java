2a0abd68718a706ad619c356a52d72eb490ce22e
邱瑞翔 <1330342818@qq.com>
2022-03-27 17:59:45 +0800

    计时器的用法

---
+++ thread/Demo6.java
@@ -0,0 +1,21 @@
+package qrxedu;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Demo6 {
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("hh");
+            }
+        },3000);
+        System.out.println("hello");
+    }
+}
