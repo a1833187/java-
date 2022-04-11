9c277634c45690184f0264cb03fff7afa190057c
邱瑞翔 <1330342818@qq.com>
2022-04-11 17:24:27 +0800

    排列组合

---
+++ lcseries/leedcode357/Solution.java
@@ -0,0 +1,31 @@
+package qrxedu.leedcode357;
+
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * 排列组合
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 10;
+        }
+        int cnt = 10, cur = 9;
+        for (int i = 0; i < n - 1; i++) {
+            cur *= 9 - i;
+            cnt += cur;
+        }
+        return cnt;
+    }
+
+    @Test
+    public void show() {
+        System.out.println(countNumbersWithUniqueDigits(3));
+    }
+}
