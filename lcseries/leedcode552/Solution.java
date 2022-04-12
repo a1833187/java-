403dd1c475f5b74dd38a6451cd1a977d1c36a01d
邱瑞翔 <1330342818@qq.com>
2022-04-12 19:21:14 +0800

    dp(学生出勤纪录II)

---
+++ lcseries/leedcode552/Solution.java
@@ -0,0 +1,39 @@
+package qrxedu.leedcode552;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    int MOD = (int)(Math.pow(10,9)+7);
+    public int checkRecord(int n) {
+        //f[i][j][k]:前i天缺勤天数为j且结尾连续迟到天数为k时的数量
+        int[][][] f = new int[n+1][2][3];
+        f[0][0][0] = 1;
+        //第i天的纪录为缺勤,f[i][1][0] += f[i-1][0][k]
+        //第i天的纪录为出勤,f[i][j][0] += f[i-1][j][k]
+        //第i天的纪录为迟到,f[i][j][k] += f[i-1][j][k-1], k <= 2
+        for(int i = 1; i <= n; i++) {
+            for (int k = 0; k < 3; k++) {
+                f[i][1][0] = (f[i][1][0] + f[i - 1][0][k]) % MOD;
+            }
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 3; k++) {
+                    f[i][j][0] = (f[i][j][0] + f[i - 1][j][k]) % MOD;
+                }
+            }
+            for (int j = 0; j < 2; j++) {
+                for (int k = 1; k < 3; k++) {
+                    f[i][j][k] = (f[i][j][k] + f[i - 1][j][k - 1]) % MOD;
+                }
+            }
+        }
+        int sum = 0;
+        for(int j = 0; j < 2; j++){
+            for(int k = 0; k < 3; k++){
+                sum= (sum + f[n][j][k])%MOD;
+            }
+        }
+        return sum;
+    }
+}
