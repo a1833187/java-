d92517330b948e97f547b4f323fcc6ee23ddb348
邱瑞翔 <1330342818@qq.com>
2022-04-18 18:51:30 +0800

    线性dp(比特数)

---
+++ lcseries/leedcode338/Solution.java
@@ -0,0 +1,24 @@
+package qrxedu.leedcode338;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public int[] countBits(int n) {
+        int[] f = new int[n+1];
+        for(int i = 1; i <= n; i++){
+            f[i] = f[i>>1] + (i%2==0 ? 0 : 1);
+        }
+        return f;
+    }
+
+    @Test
+    public void show(){
+        System.out.println(Arrays.toString(countBits(5)));
+    }
+}
