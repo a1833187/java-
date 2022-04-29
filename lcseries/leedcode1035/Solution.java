612d6c608b52b31cca56ce8e42140c35031acb8d
邱瑞翔 <1330342818@qq.com>
2022-04-29 17:28:51 +0800

    序列dp(不相交的线(LCS))

---
+++ lcseries/leedcode1035/Solution.java
@@ -0,0 +1,25 @@
+package qrxedu.leedcode1035;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int[][] f = new int[m+1][n+1];
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                int num1 = nums1[i-1];
+                int num2 = nums2[j-1];
+                if(num1 == num2){
+                    f[i][j] = f[i-1][j-1]+1;
+                }else{
+                    f[i][j] = Math.max(f[i][j-1],f[i-1][j]);
+                }
+            }
+        }
+        return f[m][n];
+    }
+}
