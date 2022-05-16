bf1e37e09bf2912965a345e4b2fcbb0dbb55a5fd
邱瑞翔 <1330342818@qq.com>
2022-05-16 16:58:21 +0800

    二分

---
+++ lcseries/leedcode1894/Solution.java
@@ -0,0 +1,30 @@
+package qrxedu.leedcode1894;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public int chalkReplacer(int[] chalk, int k_) {
+        long sum = 0;
+        long k = k_;
+        for(int num : chalk){
+            sum += num;
+        }
+        if(k % sum == 0){
+            return 0;
+        }
+        k = k % sum;
+        int n = chalk.length;
+        for(int i = 0; i < n; i++){
+            if(k >= chalk[i]){
+                k-=chalk[i];
+            }else{
+                return i;
+            }
+        }
+        return 0;
+    }
+}
