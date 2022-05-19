5496178f8749c212e1cf35b2af0610cc66d81916
邱瑞翔 <1330342818@qq.com>
2022-05-19 19:57:54 +0800

    滑动窗口(最少移动次数)

---
+++ lcseries/leedcode462/Solution.java
@@ -0,0 +1,33 @@
+package qrxedu.leedcode462;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public int minMoves2(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        int[] sum = new int[n+1];
+        for(int i = 0; i < n; i++){
+            sum[i+1] = sum[i] + nums[i];
+        }
+        long ans = Long.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            long front = sum[i]-sum[0];
+            long fear = sum[n] - sum[i+1];
+            long tmp = (long) nums[i] *i-front + fear - (long) nums[i] *(n-i-1);
+            ans = Math.min(ans,tmp);
+        }
+        return (int)ans;
+    }
+    @Test
+    public void show(){
+        int[] nums = {1,10,2,9};
+        System.out.println(minMoves2(nums));
+    }
+}
