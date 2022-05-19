d1fd11e33c4df5913b50d7ba2e0da186a3ed7e9c
邱瑞翔 <1330342818@qq.com>
2022-05-19 19:59:30 +0800

    二分(寻找比目标字母大的最小字母)

---
+++ lcseries/leedcode744/Solution.java
@@ -0,0 +1,30 @@
+package qrxedu.leedcode744;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int n = letters.length;
+        if(target >= letters[n-1]){
+            return letters[0];
+        }
+        int l = 0,r = n-1;
+        while(l < r){
+            int mid = l + r >> 1;
+            if(letters[mid] >= target){
+                r = mid;
+            }else{
+                l = mid+1;
+            }
+        }
+        return letters[l];
+    }
+    @Test
+    public void show(){
+        System.out.println(0+0>>1);
+    }
+}
