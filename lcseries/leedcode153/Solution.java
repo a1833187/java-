175ce258f2c5251bdae7bc488f44c32b3d7867a4
邱瑞翔 <1330342818@qq.com>
2022-05-20 18:00:27 +0800

    二分(旋转数组的最小值)

---
+++ lcseries/leedcode153/Solution.java
@@ -0,0 +1,25 @@
+package qrxedu.leedcode153;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        if(n == 1){
+            return nums[0];
+        }
+        int key = nums[0];
+        int l = 0, r = n - 1;
+        while(l < r){
+            int mid = l + r + 1 >> 1;
+            if(nums[mid] > key){
+                l = mid;
+            }else{
+                r = mid - 1;
+            }
+        }
+        return l == n-1 ? nums[0] : nums[l+1];
+    }
+}
