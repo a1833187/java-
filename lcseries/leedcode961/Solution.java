5792e353566ba8a88add88dd738d7c3c61fa1f20
邱瑞翔 <1330342818@qq.com>
2022-05-21 21:36:26 +0800

    排序

---
+++ lcseries/leedcode961/Solution.java
@@ -0,0 +1,22 @@
+package qrxedu.leedcode961;
+
+import java.util.Arrays;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        if(n == 2){
+            return nums[0];
+        }
+        if(nums[n/2] == nums[n/2+1]){
+            return nums[n/2];
+        }else{
+            return nums[n/2-1];
+        }
+    }
+}
