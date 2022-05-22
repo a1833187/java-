5aecd588a5d3b97254f9182c27b9792365dc3c33
邱瑞翔 <1330342818@qq.com>
2022-05-22 21:59:58 +0800

    二分(旋转数组含重复元素)

---
+++ lcseries/leedcode81/Solution.java
@@ -0,0 +1,42 @@
+package qrxedu.leedcode81;
+
+/**
+ * 旋转数组II:当含有重复元素时,时间复杂度为O(n)
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if(n == 0){
+            return false;
+        }
+        if(n == 1){
+            return nums[0] == target;
+        }
+        int l = 0,r = n - 1;
+        while(l < r){
+            int mid = l + r >> 1;
+            if(nums[mid] == target){
+                return true;
+            }
+            if(nums[mid] == nums[l] && nums[mid] == nums[r]){
+                l++;
+                r--;
+            }else if(nums[mid] >= nums[l]){
+                if(nums[l] <= target && target < nums[mid]){
+                    r = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+            }else{
+                if(target <= nums[n-1] && target > nums[mid]){
+                    l = mid + 1;
+                }else{
+                    r = mid - 1;
+                }
+            }
+        }
+        return nums[l] == target;
+    }
+}
