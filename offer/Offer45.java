d04d83a57a01df7ec96f8a3c180de8ff99e9eda7
邱瑞翔 <1330342818@qq.com>
2022-06-19 18:34:48 +0800

    剑指Offer(自定义排序)

---
+++ offer/Offer45.java
@@ -0,0 +1,30 @@
+package qrxedu.offer;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Comparator;
+
+/**
+ * @author qiu
+ * @version 1.8.0
+ */
+public class Offer45 {
+    public String minNumber(int[] nums) {
+        int n = nums.length;
+        String[] s = new String[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(s, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o1+o2).compareTo(o2+o1);
+            }
+        });
+        String ans = "";
+        for(String str : s){
+            ans += str;
+        }
+        return ans;
+    }
+}
